82cb62c6a98d9a6a7500577aad5dfdc941cf56a8
shaheerhashim10 <shaheerhashim10@gmail.com>
2021-07-18 17:14:05 +0500

    First Commit

---
+++ src/api/Requests.js
@@ -0,0 +1,16 @@
+// Typically we would store it in {process.env.API_KEY}
+
+const API_KEY = "a37077ad7c72b537055f60bc347f0e4b";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
---
+++ src/app/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBlGc876dZiXgNcFxy-976SWaJOYezfGec",
+    authDomain: "netflix-build-fd9fd.firebaseapp.com",
+    projectId: "netflix-build-fd9fd",
+    storageBucket: "netflix-build-fd9fd.appspot.com",
+    messagingSenderId: "795965294455",
+    appId: "1:795965294455:web:fc9da53d43478a96f24372"
+  };
+  
